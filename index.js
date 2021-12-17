ec4484fef445541ae541ac167180d1d2eb35219e
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-12-16 21:11:29 -0700

    commit tree 1c5c5f138999f1be7434cf1089c96b181c3c53bd

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function user(name, email){
+    let id = document.createElement('div')
+    id.innerHTML = `
+        <p>Name: ${name}</p>
+        <p>Email: ${email}</p>
+    `
+    document.querySelector('body').appendChild(id)
+}
+
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({name, email}),
+    })
+    .then(response => response.json())
+    .then(json => document.body.innerHTML = json.id)
+    .catch(function(error){
+        document.body.innerHTML = error
+    })
+}
